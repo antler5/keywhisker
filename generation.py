9bfca6e8f10d2913c35bb518f11aa4b6d9ebfb98
semi <semilin@disroot.org>
2024-07-27 18:48:07 -0700

    update generation.py

--- generation.py
+++ generation.py
@@ -2,31 +2,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-files = [('data/greedy_naive_runs.csv', '100k Naive greedy runs (5k iterations each)', (0, 0)),
-         ('data/greedy_neighbor_runs.csv', '100k Deterministic greedy runs', (0, 1)),
-         ('data/simulated_annealing_5k_runs.csv', '100k Simulated annealing runs (5k iterations each)', (1, 0)),
-         ('data/simulated_annealing_20k_runs.csv', '100k Simulated annealing runs (20k iterations each)', (1, 1)),
-         ('data/simulated_annealing_100k_runs.csv', '10k Simulated annealing runs (100k iterations each)', (2, 0)),
-         ('data/simulated_annealing_1m_runs.csv', '10k Simulated annealing runs (1m iterations each)', (2, 1))]
+# files = [('data/greedy_naive_runs.csv', '100k Naive greedy runs (5k iterations each)', (0, 0)),
+#          ('data/greedy_neighbor_runs.csv', '100k Deterministic greedy runs', (0, 1)),
+#          ('data/simulated_annealing_5k_runs.csv', '100k Simulated annealing runs (5k iterations each)', (1, 0)),
+#          ('data/simulated_annealing_20k_runs.csv', '100k Simulated annealing runs (20k iterations each)', (1, 1)),
+#          ('data/simulated_annealing_100k_runs.csv', '10k Simulated annealing runs (100k iterations each)', (2, 0)),
+#          ('data/simulated_annealing_1m_runs.csv', '10k Simulated annealing runs (1m iterations each)', (2, 1))]
 
 files = [('data/roll_greedy_deterministic_runs.tsv', 'Optimizing rolls with 10k deterministic greedy runs', 0),
          ('data/inroll_greedy_deterministic_runs.tsv', 'Optimizing inrolls with 10k deterministic greedy runs', 1),
-         ('data/e200_inroll_greedy_deterministic_runs.tsv', 'Optimizing inrolls with 1k deterministic greedy runs', 2),
-         ('data/mr_taipo_gd_sfs.tsv', 'Optimizing taipo sfs with 5k deterministic greedy runs', 3)]
+         ('data/e200_inroll_greedy_deterministic_runs.tsv', 'Optimizing inrolls with 1k deterministic greedy runs', 2)]
 
 sns.set()
 fig, axs = plt.subplots(len(files), tight_layout=True)
 fig.suptitle('Resulting Roll values over massed generation runs with various algorithms')
-for file, title, index in files:
+for (file, title, index) in files:
     ax = axs[index]
-    data = pd.read_csv(file, sep='\t')[:10000]
+    data = pd.read_csv(file)[:10000]
     minimum = data.amount.min()
     mincount = data.amount.value_counts()[minimum]
     maximum = data.amount.max()
     maxcount = data.amount.value_counts()[maximum]
     total = len(data)
-    print(title)
     print(f'  min {minimum} occurred {mincount} times ({100*mincount/total}%)')
     print(f'  max {maximum} occurred {maxcount} times ({100*maxcount/total}%)')
     print(f'  mean {data.amount.mean()}')
