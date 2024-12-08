a3a1d937db89280531634f9ae1d92b8c69adff82
antlers <antlers@illucid.net>
2024-12-08 14:23:52 -0800

    3D.py: Add new script for 3D plots.
    The class-based refactor leaves it a little unclear whether `fig`'s
    creation and `plt.show()` should be in the `plot` method or in
    "`__main__`". It's different between the two scripts because I had a
    little trouble with `fig` in `3D.py`. This is okay for now.
    
    * bin/3D.py: New file.
    * bin/heatmap.py: Refactor into classes.

---
+++ bin/3D.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env -S python -m pipx run
+
+# SPDX-FileCopyrightText: 2024 antlers <antlers@illucid.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#   "docopt",
+#   "matplotlib",
+#   "numpy",
+#   "pandas",
+#   "scipy"
+# ]
+# ///
+
+"""Create a heatmap of rows from FILE on an X by Y by Z grid.
+
+FILE should be a TSV.
+X, Y, and Z should specify the labels of three numerical columns.
+
+Usage:
+  3D.py [options] FILE
+
+  -x X               X axis column label. [default: sfb]
+  -y Y               Y axis column label. [default: sfs]
+  -z Z               Z axis column label. [default: m3roll]
+  -t, --title TITLE  Set graph title
+  -o, --out OUT      Output file. [default: 3D.gif]
+  -S, --score        Color by mean score instead of density.
+  -A, --animate      Rotate azimuth.
+  --hide-best        Don't label "best" layout.
+"""
+
+from docopt import docopt
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+
+from matplotlib import animation
+from matplotlib.colors import LogNorm
+
+from heatmap import HeatmapContext
+
+def process_bin(bin_indices, data, x_centers, y_centers, z_centers, x, y, z, score_threshold):
+    """Process a single bin, returning median score if below threshold."""
+    x_bin, y_bin, z_bin = bin_indices
+    x_range = (x_centers[x_bin] - (x_centers[1] - x_centers[0]) / 2, x_centers[x_bin] + (x_centers[1] - x_centers[0]) / 2)
+    y_range = (y_centers[y_bin] - (y_centers[1] - y_centers[0]) / 2, y_centers[y_bin] + (y_centers[1] - y_centers[0]) / 2)
+    z_range = (z_centers[z_bin] - (z_centers[1] - z_centers[0]) / 2, z_centers[z_bin] + (z_centers[1] - z_centers[0]) / 2)
+
+    bin_points = data[(x >= x_range[0]) & (x < x_range[1]) &
+                      (y >= y_range[0]) & (y < y_range[1]) &
+                      (z >= z_range[0]) & (z < z_range[1])]
+
+    if not bin_points.empty:
+      median_score = bin_points['score'].median()
+      if median_score <= score_threshold:
+        return median_score, (x_bin, y_bin, z_bin)
+    return None
+
+class Histogram3D(HeatmapContext):
+  def __init__(self, args):
+    super().__init__(args, 3)
+    if self.score:
+      self.density_threshold_percentile=92
+    else:
+      self.density_threshold_percentile=96
+    self.jitter_strength=0.25
+    self.args = args
+
+  def bin_data(self, num_bins=40):
+    "Bin the data into a 40^3 grid based on min and max values."
+    x_min, x_max = self.x.min(), self.x.max()
+    y_min, y_max = self.y.min(), self.y.max()
+    z_min, z_max = self.z.min(), self.z.max()
+    score_min, score_max = self.data['score'].min(), self.data['score'].max()
+
+    x_bins = np.linspace(x_min, x_max, num_bins + 1)
+    y_bins = np.linspace(y_min, y_max, num_bins + 1)
+    z_bins = np.linspace(z_min, z_max, num_bins + 1)
+
+    hist, edges = np.histogramdd(
+      sample=[self.x, self.y, self.z],
+      bins=[x_bins, y_bins, z_bins])
+
+    x_centers = 0.5 * (edges[0][:-1] + edges[0][1:])
+    y_centers = 0.5 * (edges[1][:-1] + edges[1][1:])
+    z_centers = 0.5 * (edges[2][:-1] + edges[2][1:])
+
+    return hist, x_centers, y_centers, z_centers
+
+  def plot(self) -> tuple:
+    hist, x_centers, y_centers, z_centers = self.bin_data(num_bins=40)
+
+    if self.score:
+      # Get bins where the score is above the threshold
+      score_threshold = np.percentile(self.data['score'], self.density_threshold_percentile)
+      with mp.Pool(mp.cpu_count()) as pool:
+        results = pool.starmap(process_bin, [(bin_indices, self.data, x_centers, y_centers, z_centers, self.x, self.y, self.z, score_threshold) for bin_indices in np.ndindex(hist.shape)])
+
+      color_values = [res[0] for res in results if res]
+      non_empty_bins = [res[1] for res in results if res]
+      non_empty_bins = np.array(non_empty_bins).T
+
+      cmap = 'inferno_r'
+    else:
+      # Filter based on density percentiles (non-multiprocessed)
+      density_threshold = np.percentile(hist.flatten(), self.density_threshold_percentile)
+      non_empty_bins = np.where(hist >= density_threshold)
+      color_values = hist[non_empty_bins]
+      norm = LogNorm()
+
+    # Jitter positions of bins to avoid perfect alignment
+    for d in self.dims:
+      setattr(self, 'jitter_' + d, np.random.uniform(-self.jitter_strength, self.jitter_strength, size=len(non_empty_bins[0])))
+
+    # Scatter the non-empty bins with color_values based on selected color option
+    plt.style.use('dark_background')
+    self.fig = plt.figure(figsize=(12,8))
+    self.ax = self.fig.add_subplot(projection='3d')
+
+    # Customize plot
+    self.ax.set_proj_type('persp', focal_length=0.2)
+    self.set_axes_labels()
+    for d in c.dims:
+      getattr(self.ax, d + 'axis').set_pane_color((1.0, 1.0, 1.0, 0.0))
+    self.ax.set_facecolor('black')
+    self.fig.set_facecolor('black')
+    self.ax.grid(False)
+
+    scatter = self.ax.scatter(
+      x_centers[non_empty_bins[0]] + self.jitter_x,
+      y_centers[non_empty_bins[1]] + self.jitter_y,
+      z_centers[non_empty_bins[2]] + self.jitter_z,
+      c=color_values,
+      cmap=cmap if 'cmap' in locals() else None,
+      norm=norm if 'norm' in locals() else None,
+      alpha=0.8,
+      s=((color_values - np.mean(color_values)) / np.std(color_values)) + 2 * 5)
+
+    self.label_known_layouts()
+
+    cbar = c.fig.colorbar(mappable=scatter) # type: ignore[arg-type,index]
+
+    # XXX: Covers up the cbar label :c
+    # cbar.ax.set_position([0.91, 0.1, 0.03, 0.8])
+
+    cbar.ax.invert_yaxis()
+    if c.score:
+      cbar.set_label("Mean score / bin\n(brighter is better)")
+    else:
+      cbar.set_label("Generated layout density / bin")
+
+    if self.title:
+      plt.title(self.title)
+    elif self.score:
+      plt.title(f'Top {self.density_threshold_percentile}th Percentile by Score')
+    else:
+      plt.title(f'Top {self.density_threshold_percentile}th Percentile by Density')
+
+    if self.args['--animate']:
+      def animate(i):
+        self.ax.view_init(elev=10, azim=i)
+        return scatter,
+      ani = animation.FuncAnimation(self.fig, animate, frames=np.arange(0, 360, 2), interval=1000, blit=True)
+      writer = animation.PillowWriter(fps=15, bitrate=1800)
+      ani.save(self.out, writer=writer)
+
+    else:
+      plt.show()
+
+# === Main ===
+if __name__ == "__main__":
+  args = docopt(__doc__)
+
+  c = Histogram3D(args)
+
+  # Visualize the top percentile of the data
+  c.plot()
--- bin/heatmap.py
+++ bin/heatmap.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S python -m pipx run
 
+# SPDX-FileCopyrightText: 2024 antlers <antlers@illucid.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
@@ -10,6 +13,8 @@
 # ]
 # ///
 
+# mypy: disable-error-code="attr-defined"
+
 """Create a heatmap of rows from FILE on an X by Y grid.
 
 FILE should be a TSV.
@@ -18,19 +23,20 @@ X and Y should specify the labels of two numerical columns.
 Usage:
   heatmap.py [options] FILE
 
-  -x X         X axis column label. [default: m3roll]
-  -y Y         Y axis column label. [default: sfb]
-  -t TITLE     Graph title. [default: 2x4 w/ Thumb]
-  -o OUT       Output file. [default: img.svg]
-  -H           Use hexbins instead of hist2d.
-  -S           Color by mean score instead of density.
-  --hide-best  Don't label "best" layout.
+  -x X               X axis column label. [default: m3roll]
+  -y Y               Y axis column label. [default: sfb]
+  -t, --title TITLE  Graph title. [default: 2x4 w/ Thumb]
+  -o, --out OUT      Output file. [default: img.svg]
+  -H, --hex          Use hexbins instead of hist2d.
+  -S, --score        Color by mean score instead of density.
+  --hide-best        Don't label "best" layout.
 """
 
 # === Imports ===
 import sys
 
-from typing import Any, Literal
+from abc import ABC, abstractmethod
+from typing import Any, Final, Literal
 
 from docopt import docopt
 
@@ -57,21 +63,21 @@ def row(m3roll: float, sfb: float, sfs: float) -> dict:
   }
 
 known_layouts: dict = {
-  # 'ardux':            row(1.32, 92.26, 90.93),
-  'ardux-no-spc':     row(2.92, 54.56, 58.45),
-  # 'artsey':           row(1.38, 90.90, 90.19),
-  'artsey-no-spc':    row(3.02, 53.16, 58.23),
-  'caret':            row(5.72, 52.58, 50.16),
-  'caret-no-spc':     row(9.92, 34.25, 50.20),
-  'taipo':            row(9.69, 31.11, 33.85),
-  'taipo-no-spc':     row(4.31, 49.18, 51.38),
-  '1-taurine':        row(14.41, 28, 38.73),
-  '2': row(18.03, 27.88, 42.89),
-  '3': row(12.96, 28.09, 36.39),
-  '4': row(9.98, 28.54, 37.18),
-  '5': row(15.015007, 30.14812, 37.49303),
-  '6': row(15.273136,32.06765,36.43247),
-  '7': row(12.924261,24.590355,43.340397)
+  # 'ardux':         row(1.32, 92.26, 90.93),
+  'ardux-no-spc':  row(2.92, 54.56, 58.45),
+  # 'artsey':        row(1.38, 90.90, 90.19),
+  # 'artsey-no-spc': row(3.02, 53.16, 58.23),
+  # 'caret':         row(5.72, 52.58, 50.16),
+  # 'caret-no-spc':  row(9.92, 34.25, 50.20),
+  'taipo':         row(9.69, 31.11, 33.85),
+  'taipo-no-spc':  row(4.31, 49.18, 51.38),
+  '1-taurine':     row(14.41, 28, 38.73),
+  '2':             row(18.03, 27.88, 42.89),
+  '3':             row(12.96, 28.09, 36.39),
+  '4':             row(9.98, 28.54, 37.18),
+  '5':             row(15.015007, 30.14812, 37.49303),
+  '6':             row(15.273136,32.06765,36.43247),
+  '7':             row(12.924261,24.590355,43.340397)
 }
 
 # === Helpers ===
@@ -79,132 +85,153 @@ def maybe_upper(s: str) -> str:
   "Uppercase string S if it doesn't have any numbers in it."
   return s.upper() if s.isalpha() else s
 
-def load_data(args: dict) -> pd.DataFrame:
-  "Read file from args and add known layouts."
-  data = pd.read_csv(args['FILE'], sep='\t')
+class HeatmapContext(ABC):
+  def __init__(self, args, dim: int) -> None:
+    for k, v in args.items():
+      setattr(self, k.lstrip('-').lower().replace('-', '_'), v)
 
-  # Concat known layouts into data, setting any missing columns to NaN.
-  if not args['--hide-best']:
-    known_layouts['0-best'] = data.loc[data['score'].idxmin()]
-  df = pd.DataFrame(known_layouts.values())
-  for col in data.columns.difference(df.columns):
-    df[col] = np.nan
-  data = pd.concat([data, df], ignore_index=True)
+    self.load_data()
 
-  return data
+    self.dim = dim
+    self.dims = ['x','y','z'][:dim]
 
-def set_axes_labels(args: dict, ax: matplotlib.axes._axes.Axes, dim: int) -> None:
-  "Set and style axis labels from args."
-  ax.set_facecolor('black')
-  for i in range(0,dim):
-    dim_letter = ['x','y','z'][i]
+    for d in self.dims:
+      label = getattr(self, d)
+      data = self.data[label]
+      setattr(self, d + 'label', label)
+      setattr(self, d, data)
 
-    set_label = getattr(ax, 'set_' + dim_letter + 'label')
-    set_label(maybe_upper(args['-' + dim_letter]))
+  def load_data(self) -> None:
+    "Read file from args and add known layouts."
+    data = pd.read_csv(self.file, sep='\t')
 
-    set_major_formatter = getattr(ax, dim_letter + 'axis').set_major_formatter
-    set_major_formatter(PercentFormatter(decimals=0))
+    # Concat known layouts into data, setting any missing columns to NaN.
+    if not self.hide_best:
+      known_layouts['0-best'] = data.loc[data['score'].idxmin()]
+    df = pd.DataFrame(known_layouts.values())
+    for col in data.columns.difference(df.columns):
+      df[col] = np.nan
+    data = pd.concat([data, df], ignore_index=True)
 
-def label_known_layouts(args: dict, ax: matplotlib.axes._axes.Axes, dim: int) -> None:
-  "Label known layouts on Axes AX."
-  for layout, metrics in known_layouts.items():
-    color = 'red' if layout[0].isdigit() else 'orange'
-    pos: list[float] = []
+    self.data = data
 
-    for i in range(0,dim):
-      dim_letter = ['x','y','z'][i]
-      pos += [metrics[args['-' + dim_letter]]]
+  def set_axes_labels(self) -> None:
+    "Set and style axis labels from args."
+    for d in self.dims:
+      set_label = getattr(self.ax, 'set_' + d + 'label')
+      set_label(maybe_upper(getattr(self, d + 'label')))
 
-    ax.scatter(
-      *pos, # type: ignore[arg-type,misc]
-      label=layout,
-      marker='o',
-      s=25, color=color,
-      edgecolor='#555'
-    )
+      set_major_formatter = getattr(self.ax, d + 'axis').set_major_formatter
+      set_major_formatter(PercentFormatter(decimals=0))
 
-    ax.text( # type: ignore[call-arg]
-      *(pos[0]+0.35, *pos[1:]), layout, # type: ignore[arg-type]
-      va='center', color='black', fontsize=8,
-      bbox=dict(facecolor='#D9D9D9', edgecolor='#555', boxstyle='round')
-    )
+  def label_known_layouts(self) -> None:
+    "Label known layouts on Axes AX."
+    for layout, metrics in known_layouts.items():
+      color = 'red' if layout[0].isdigit() else 'orange'
+      pos: list[float] = []
 
-def make_hist(args: dict, ax: matplotlib.axes._axes.Axes) -> Any:
-  (x, y) = (data[args['-x']], data[args['-y']])
-  hist: Any
+      for d in self.dims:
+        pos += [metrics[getattr(self, d + 'label')]]
 
-  if args['-S']:
-    cmap = 'inferno_r'
+      self.ax.scatter(
+        *pos, # type: ignore[arg-type,misc]
+        label=layout,
+        marker='o',
+        s=25, color=color,
+        edgecolor='#555')
 
-  if args['-H']:
-    if args['-S']:
-      C = data['score']
-      reduce_C_function = np.mean
-    else:
-      bins: Literal['log'] = 'log'
-      mincnt = 1
+      self.ax.text( # type: ignore[call-arg]
+        *(pos[0]+0.35, *pos[1:]), layout, # type: ignore[arg-type]
+        va='center', color='black', fontsize=8,
+        bbox=dict(facecolor='#D9D9D9', edgecolor='#555', boxstyle='round'))
 
-    hist = ax.hexbin(
-      x, y,
-      gridsize=(50,30), linewidths=0,
-      C=C if 'C' in locals() else None,
-      bins=bins if 'bins' in locals() else None,
-      cmap=cmap if 'cmap' in locals() else None,
-      mincnt=mincnt if 'mincnt' in locals() else None,
-      reduce_C_function=reduce_C_function if 'reduce_C_function' in locals() else None) # type: ignore[arg-type]
-    return (hist, hist)
+  @abstractmethod
+  def plot(self) -> tuple:
+    pass
 
-  else:
+class Histogram2D(HeatmapContext):
+  def __init__(self, args):
+    super().__init__(args, 2)
 
-    if args['-S']:
-      # Create bins
-      x_bins, y_bins = (64, 64)
-      x_edges = np.linspace(x.min(), x.max(), x_bins + 1)
-      y_edges = np.linspace(y.min(), y.max(), y_bins + 1)
-      hist , xedges, yedges = np.histogram2d(x, y, bins=[x_edges, y_edges])
+  def plot(self) -> tuple:
+    hist: Any
 
-      # Calculate means
-      bin_means = np.zeros_like(hist)
-      for i in range(x_bins):
-        for j in range(y_bins):
-          # Get indexes of points in bin
-          mask = ((x >= xedges[i]) & (x < xedges[i + 1]) &
-                  (y >= yedges[j]) & (y < yedges[j + 1]))
-          bin_means[i, j] = data.loc[mask, 'score'].mean() if mask.any() else np.nan
+    if self.score:
+      cmap = 'inferno_r'
 
-      # Couldn't get hist2d to cooperate by setting `weights`, `pcolormesh` will do.
-      hist = plt.pcolormesh(x_edges, y_edges, bin_means.T, cmap=cmap)
+    if self.hex:
+      if self.score:
+        C = self.data['score']
+        reduce_C_function = np.mean
+      else:
+        bins: Literal['log'] = 'log'
+        mincnt = 1
+
+      hist = self.ax.hexbin(
+        self.x, self.y,
+        gridsize=(50,30), linewidths=0,
+        C=C if 'C' in locals() else None,
+        bins=bins if 'bins' in locals() else None,
+        cmap=cmap if 'cmap' in locals() else None,
+        mincnt=mincnt if 'mincnt' in locals() else None,
+        reduce_C_function=reduce_C_function if 'reduce_C_function' in locals() else None) # type: ignore[arg-type]
       return (hist, hist)
 
     else:
-      hist = ax.hist2d(x, y, bins=64, norm=LogNorm())
-      return (hist, hist[3])
+      if self.score:
+        # Couldn't color by score by setting hist2d's `weights`, so I'll do the
+        # math myself and call `pcolormesh`.
+
+        # Create bins
+        x_bins, y_bins = (64, 64)
+        x_edges = np.linspace(self.x.min(), self.x.max(), x_bins + 1)
+        y_edges = np.linspace(self.y.min(), self.y.max(), y_bins + 1)
+        hist , xedges, yedges = np.histogram2d(self.x, self.y, bins=[x_edges, y_edges]) # type: ignore[list-item]
+
+        # Calculate means
+        bin_means = np.zeros_like(hist)
+        for i in range(x_bins):
+          for j in range(y_bins):
+            # Get indexes of points in bin
+            mask = ((self.x >= xedges[i]) & (self.x < xedges[i + 1]) &
+                    (self.y >= yedges[j]) & (self.y < yedges[j + 1]))
+            bin_means[i, j] = self.data.loc[mask, 'score'].mean() if mask.any() else np.nan
+
+        hist = plt.pcolormesh(x_edges, y_edges, bin_means.T, cmap=cmap)
+        return (hist, hist)
+      else:
+        hist = self.ax.hist2d(self.x, self.y, bins=64, norm=LogNorm())
+        return (hist, hist[3])
 
 # === Main ===
 if __name__ == "__main__":
   args = docopt(__doc__)
+  c = Histogram2D(args)
 
-  data = load_data(args)
-
+  c.fig: plt.Figure
+  c.ax: plt.Axes
   fig, ax = plt.subplots(tight_layout=True)
-  fig.patch.set_facecolor('#D9D9D9')
+  setattr(c, 'fig', fig)
+  setattr(c, 'ax', ax)
+  c.fig.patch.set_facecolor('#D9D9D9')
+  ax.set_facecolor("black")
 
-  hist, mappable = make_hist(args, ax)
+  hist, mappable = c.plot()
 
-  set_axes_labels(args, ax, 2)
-  label_known_layouts(args, ax, 2)
+  c.set_axes_labels()
+  c.label_known_layouts()
 
   # Colorbar
-  cbar = fig.colorbar(mappable=mappable, ax=ax) # type: ignore[arg-type,index]
+  cbar = c.fig.colorbar(mappable=mappable, ax=c.ax) # type: ignore[arg-type,index]
 
-  if args['-S']:
+  if c.score:
     cbar.ax.invert_yaxis()
-    cbar.set_label("Mean score / cell\n(lower/brighter is better)")
+    cbar.set_label("Mean score / cell\n(brighter is better)")
   else:
     cbar.set_label("Generated layout density\n(per ~1/64²)")
 
   # Export
-  plt.title(args['-t'] + "\n" + f'({len(data):,} samples)')
-  plt.savefig(args['-o'], dpi=180)
+  plt.title(c.title + "\n" + f'({len(c.data):,} samples)')
+  plt.savefig(c.out, dpi=180)
 
-  print("Created", args['-o'])
+  print("Created", c.out)
